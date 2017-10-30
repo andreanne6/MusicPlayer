1f2fe219ab4982870b555a326090d1a5aff8ff08
Andreanne Laurin <anlaurin@IAD.CA.INET>
2017-10-30 11:41:56 -0400

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, Directive } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  // directives: [ROUTER_DIRECTIVES]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- // title = 'app works!';
+ title = 'app works!';
 }
 
